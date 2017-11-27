834295855453bc607c9a13dcce6b1f2f38afd885
Pawel Duda <pawel.duda91@gmail.com>
2017-11-28 00:02:02 +0100

    WIP

--- lib/tesla/adapter/shared.ex
+++ lib/tesla/adapter/shared.ex
@@ -1,6 +1,19 @@
 defmodule Tesla.Adapter.Shared do
   @moduledoc false
-  
+
+  def capture_query_params(%Tesla.Env{method: :get} = env) do
+    if String.contains?(env.url, "?") do
+      query_params =
+        (env.url |> URI.parse()).query |> URI.decode_query() #|> Map.to_list()
+        # query_params = for {key, val} <- query_params, into: [], do: {String.to_atom(key), val}
+
+      %{env | query: query_params}
+    else
+      env
+    end
+  end
+  def capture_query_params(env), do: env
+
   def stream_to_fun(stream) do
     reductor = fn(item, _acc) -> {:suspend, item} end
     {_, _, fun} = Enumerable.reduce(stream, {:suspend, nil}, reductor)
--- test/support/adapter_case/basic.ex
+++ test/support/adapter_case/basic.ex
@@ -77,6 +77,26 @@ defmodule Tesla.AdapterCase.Basic do
           assert args["user[age]"]  == "20"
         end
 
+        test "capturing query params from url/path" do
+          request = %Env{
+            method: :get,
+            url: "#{@url}/get?page=1&sort=desc&status[]=a&status[]=b&status[]=c&user[name]=Jon&user[age]=20"
+            # url: "#{@url}/get?page=1"
+          }
+
+          assert %Env{} = response = call(request)
+
+          response = Tesla.Middleware.JSON.decode(response, [])
+
+          query = response.query
+
+          assert query["page"] == "1"
+          assert query["sort"] == "desc"
+          # assert query["status[]"]   == ["a", "b", "c"]
+          assert query["user[name]"] == "Jon"
+          assert query["user[age]"]  == "20"
+        end
+
         test "autoredirects disabled by default" do
           request = %Env{
             method: :get,
